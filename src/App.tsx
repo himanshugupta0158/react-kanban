85115904766f14a3a0aef23596bfd9848b52d0e2
himanshugupta0158 <himanshugupta0158@gmail.com>
2024-07-21 21:23:16 +0530

    added basic card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
 
 function App() {
-
+  const title = "Hello World"
+  const id = "BUS-1"
+  const points = 5
   return (
-    <div className='text-4xl text-red-600'>
-      Hello
+    <div className='border rounded-lg p-2 m-2 bg-gray-100'>
+      <div className='text-base font-semibold py-2'>
+        {title}
+      </div>
+      <div className='flex gap-4 justify-between py-2 text-gray-700'>
+        <div>{id}</div>
+        <div>{points}</div>
+      </div>
+
     </div>
   )
 }
